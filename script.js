5b22c29a854ab4cd164c9c9bdacb1879d103b0b0
Tibor Végh <73848390+photovegh@users.noreply.github.com>
2021-11-16 14:32:47 +0100

    bootstsrap start

--- script.js
+++ script.js
@@ -1,222 +1,258 @@
-let gameOver=1;
-let jel= " ";
+let gameOver = 1;
+let jel = " ";
 let i = 0;
-let keres=true;
-let talalat=0;
-let ycoor=0;
-let xcoor=0;
+let keres = true;
+let talalat = 0;
+let ycoor = 0;
+let xcoor = 0;
 let player = [1];
 player[0] = "X";
 player[1] = "O";
 let actualPlayer = 0;
-let actualRound = 0;//MOD/2 függv-> a kör aktuális játékosa 1,2,1,... 
-let playerName = [1];// teszt miatt állandó, ha számít a név -> input text
-playerName[0]=("Tom");
-playerName[1]=("Jerry")
+let actualRound = 0; //MOD/2 fÃ¼ggv-> a kÃ¶r aktuÃ¡lis jÃ¡tÃ©kosa 1,2,1,...
+let playerName = [1]; // teszt miatt Ã¡llandÃ³, ha szÃ¡mÃ­t a nÃ©v -> input text
+playerName[0] = "Tom";
+playerName[1] = "Jerry";
 let actualPlayerName = [4];
-actualPlayerName[0] = ("Te vagy Tom   O            ");
-actualPlayerName[1] = ("Te vagy Jerry X            ");
-actualPlayerName[2] = ("Nem kattinthatsz ugyanarra!");
-actualPlayerName[3] = ("A nyertes:                 ");
+actualPlayerName[0] = "Te vagy Tom   O            ";
+actualPlayerName[1] = "Te vagy Jerry X            ";
+actualPlayerName[2] = "Nem kattinthatsz ugyanarra!";
+actualPlayerName[3] = "A nyertes:                 ";
 $("#pl0").text(playerName[0]);
 $("#pl1").text(playerName[1]);
 $("#gameGo").text(actualPlayerName[actualPlayer]);
-//player field index (0 - 99) & feltölt +
-let data = [];//vizsgálathoz, hogy foglalt-e a mezo
+//player field index (0 - 99) & feltÃ¶lt +
+let data = []; //vizsgÃ¡lathoz, hogy foglalt-e a mezo
 for (i = 0; i < 100; i++) {
-  data.push("+");
+    data.push("+");
 }
-//tábla az 5 találat vizsgálatához
-// 10 x 10-es tábla, (+1 mezo a szélekhez, vizsgálatnál ne szaladjon ki a tömbbol)
+//tÃ¡bla az 5 talÃ¡lat vizsgÃ¡latÃ¡hoz
+// 10 x 10-es tÃ¡bla, (+1 mezo a szÃ©lekhez, vizsgÃ¡latnÃ¡l ne szaladjon ki a tÃ¶mbbol)
 playGround = new Array(12);
-for (i=0; i < playGround.length; ++i){
-   playGround[i] = new Array(12);
-	
-   for (j=0; j<playGround[i].length; ++j){
-      playGround[i][j]="+";
-   }
+for (i = 0; i < playGround.length; ++i) {
+    playGround[i] = new Array(12);
+
+    for (j = 0; j < playGround[i].length; ++j) {
+        playGround[i][j] = "+";
+    }
 }
 
-//mezok felrajzolása, minden egyes elem kap külön id-t -> b0,b1,b2... és egy indexet
+//mezok felrajzolï¿½sa, minden egyes elem kap kï¿½lï¿½n id-t -> b0,b1,b2... ï¿½s egy indexet
 let index = 0;
 for (index = 0; index < 100; index++) {
-  $('#container').append(`<div class="box" id="b${index}" data-index="${index}"></div>`)
-  //  (data-index=${index})  
+    $("#container").append(
+        `<div class="box" id="b${index}" data-index="${index}"></div>`
+    );
+    //  (data-index=${index})
 }
 
 //jQuery(document).ready(function(){
 
-$('.box').click((event) => {
+$(".box").click((event) => {
+    if (gameOver == 1) {
+        //click esemï¿½ny kezelï¿½se START
 
-	if (gameOver==1){//click esemény kezelése START
-	
-  let indexClicked = $(event.target).attr('data-index');//a rákattintott mezo data-index atributumának a kiolvasása
-  let numberIndex = parseInt(indexClicked);//biztos ami biztos számmá alakítom
-  console.log(numberIndex);
-  console.log(data[numberIndex])//rákattintoot mezo tartalma +, X, O
-//**********************************************if start	
-  if (data[numberIndex] == "+") {//ha a mezo még nem használt (+) feltölt O vagy X
+        let indexClicked = $(event.target).attr("data-index"); //a rï¿½kattintott mezo data-index atributumï¿½nak a kiolvasï¿½sa
+        let numberIndex = parseInt(indexClicked); //biztos ami biztos szï¿½mmï¿½ alakï¿½tom
+        console.log(numberIndex);
+        console.log(data[numberIndex]); //rï¿½kattintoot mezo tartalma +, X, O
+        //**********************************************if start
+        if (data[numberIndex] == "+") {
+            //ha a mezo mï¿½g nem hasznï¿½lt (+) feltï¿½lt O vagy X
 
-	  $("#gameGo").text(actualPlayerName[actualPlayer]);
-    //console.log(actualPlayerName[actualPlayer]);
-    data[numberIndex] = player[actualPlayer];// aktuális játékos jele: X O
-	console.log("elemsorszám: "+numberIndex);
-// dimension koordináták kiszámolása MOD függvénnyel
-	  xcoor=numberIndex % 10;
-	  ycoor=((numberIndex-xcoor)/10);
-	  xcoor++;//hozzá kell adni 1-et, hogy a vizsgálatnál ne szaladjon ki a tömbbol (1-10)
-	  console.log("xcoor: "+xcoor);
-	  ycoor++;//hozzá kell adni 1-et, hogy a vizsgálatnál ne szaladjon ki a tömbbol (1-10)
-	  console.log("ycoor: "+ycoor);
-//tábla mezok  
-	  
-	  playGround[xcoor][ycoor]=data[numberIndex]//akt. játékos jele elhelyezése a táblába, szélso mezok kihagyva
-	  
-	  //console.log(data[numberIndex]);
-    $('#clicked').text(data[numberIndex]);//teszthez használt kiiratás képernyore source code-ban kikommentelve
-//aktuális játékos kiiratása a képernyore
-//az éppen kattintott mezo szinezése, és a megfelelo jel kirakása
-	  if (actualPlayer == 0) {
-      $("#b" + numberIndex + ".box").css("background-color", "hotpink");
-      $("#b" + numberIndex + ".box").text("X")
-      actualRound++;
+            $("#gameGo").text(actualPlayerName[actualPlayer]);
+            //console.log(actualPlayerName[actualPlayer]);
+            data[numberIndex] = player[actualPlayer]; // aktuï¿½lis jï¿½tï¿½kos jele: X O
+            console.log("elemsorszï¿½m: " + numberIndex);
+            // dimension koordinï¿½tï¿½k kiszï¿½molï¿½sa MOD fï¿½ggvï¿½nnyel
+            xcoor = numberIndex % 10;
+            ycoor = (numberIndex - xcoor) / 10;
+            xcoor++; //hozzï¿½ kell adni 1-et, hogy a vizsgï¿½latnï¿½l ne szaladjon ki a tï¿½mbbol (1-10)
+            console.log("xcoor: " + xcoor);
+            ycoor++; //hozzï¿½ kell adni 1-et, hogy a vizsgï¿½latnï¿½l ne szaladjon ki a tï¿½mbbol (1-10)
+            console.log("ycoor: " + ycoor);
+            //tï¿½bla mezok
+
+            playGround[xcoor][ycoor] = data[numberIndex]; //akt. jï¿½tï¿½kos jele elhelyezï¿½se a tï¿½blï¿½ba, szï¿½lso mezok kihagyva
+
+            //console.log(data[numberIndex]);
+            $("#clicked").text(data[numberIndex]); //teszthez hasznï¿½lt kiiratï¿½s kï¿½pernyore source code-ban kikommentelve
+            //aktuï¿½lis jï¿½tï¿½kos kiiratï¿½sa a kï¿½pernyore
+            //az ï¿½ppen kattintott mezo szinezï¿½se, ï¿½s a megfelelo jel kirakï¿½sa
+            if (actualPlayer == 0) {
+                $("#b" + numberIndex + ".box").css(
+                    "background-color",
+                    "hotpink"
+                );
+                $("#b" + numberIndex + ".box").text("X");
+                actualRound++;
+            }
+            if (actualPlayer == 1) {
+                $("#b" + numberIndex + ".box").css(
+                    "background-color",
+                    "cornflowerblue"
+                );
+                $("#b" + numberIndex + ".box").text("O");
+                actualRound++;
+            }
+            actualPlayer = actualRound % 2; //az aktuï¿½lis jï¿½tï¿½kos 0 vagy 1
+        } else {
+            //ha mï¿½r felhasznï¿½lt a mezo figy. ï¿½zenet
+            $("#gameGo").text(actualPlayerName[2]);
+        }
+
+        //Talï¿½latviszgï¿½lat ***********************************
+        keres = true;
+        novel = 1; //zero novel 2x talalja sajat magat !
+        talalat = 1;
+        jel = data[numberIndex];
+        console.log("jel. " + jel);
+        console.log(
+            "jel-jel: " +
+                playGround[ycoor][xcoor + i] +
+                " <---> " +
+                jel +
+                " szamlalo  " +
+                novel +
+                " novelt vizsgalando: " +
+                (xcoor + novel)
+        );
+
+        while (keres == true) {
+            //talï¿½latvizsgï¿½lat ciklus
+            //***********************************************************   keres ciklus start
+
+            //--------------------------------------------------------------------------------
+            ///////keres jobbra xcoor+1 !!! tehï¿½t balra tedd az elemet !!!
+            while (playGround[xcoor + novel][ycoor] == jel) {
+                talalat++;
+                novel++;
+                if (talalat == 5) {
+                    console.log(
+                        "**************+++++++++++++******* HEUREKAAA *****"
+                    );
+                    gameOver = 0;
+                }
+            }
+            novel = 1;
+            ///////keres ballra xcoor-1
+            while (playGround[xcoor - novel][ycoor] == jel) {
+                talalat++;
+                novel++;
+                if (talalat == 5) {
+                    console.log(
+                        "**************+++++++++++++******* HEUREKAAA *****"
+                    );
+                    gameOver = 0;
+                }
+            }
+            console.log("talalat: " + talalat + "  keres: " + keres);
+            keres = false;
+            console.log("talalat: " + talalat + "  keres: " + keres);
+            novel = 1;
+            talalat = 1;
+            //--------------------------------------------------------------------------------
+            //--------------------------------------------------------------------------------
+            ///////keres fugg fel ycoor+1
+            while (playGround[xcoor][ycoor + novel] == jel) {
+                talalat++;
+                novel++;
+                if (talalat == 5) {
+                    console.log(
+                        "**************+++++++++++++******* HEUREKAAA *****"
+                    );
+                    gameOver = 0;
+                }
+            }
+            novel = 1;
+            ///////keres fugg le ycoor-1
+            while (playGround[xcoor][ycoor - novel] == jel) {
+                talalat++;
+                novel++;
+                if (talalat == 5) {
+                    console.log(
+                        "**************+++++++++++++******* HEUREKAAA *****"
+                    );
+                    gameOver = 0;
+                }
+            }
+            console.log("talalat: " + talalat + "  keres: " + keres);
+            keres = false;
+            console.log("talalat: " + talalat + "  keres: " + keres);
+            novel = 1;
+            talalat = 1;
+            //--------------------------------------------------------------------------------
+            //--------------------------------------------------------------------------------
+            ///////keres jobbra le
+            while (playGround[xcoor + novel][ycoor - novel] == jel) {
+                talalat++;
+                novel++;
+                if (talalat == 5) {
+                    console.log(
+                        "**************+++++++++++++******* HEUREKAAA *****"
+                    );
+                    gameOver = 0;
+                }
+            }
+            novel = 1;
+            ///////keres ballra fel
+            while (playGround[xcoor - novel][ycoor + novel] == jel) {
+                talalat++;
+                novel++;
+                if (talalat == 5) {
+                    console.log(
+                        "**************+++++++++++++******* HEUREKAAA *****"
+                    );
+                }
+            }
+            console.log("talalat: " + talalat + "  keres: " + keres);
+            keres = false;
+            console.log("talalat: " + talalat + "  keres: " + keres);
+            novel = 1;
+            talalat = 1;
+            //--------------------------------------------------------------------------------
+            //--------------------------------------------------------------------------------
+            ///////keres ballra le
+            while (playGround[xcoor - novel][ycoor - novel] == jel) {
+                talalat++;
+                novel++;
+                if (talalat == 5) {
+                    console.log(
+                        "**************+++++++++++++******* HEUREKAAA *****"
+                    );
+                    gameOver = 0;
+                }
+            }
+            novel = 1;
+            ///////keres jobbra fel
+            while (playGround[xcoor + novel][ycoor + novel] == jel) {
+                talalat++;
+                novel++;
+                if (talalat == 5) {
+                    console.log(
+                        "**************+++++++++++++******* HEUREKAAA *****"
+                    );
+                    gameOver = 0;
+                }
+            }
+            console.log("talalat: " + talalat + "  keres: " + keres);
+            keres = false;
+            console.log("talalat: " + talalat + "  keres: " + keres);
+            novel = 1;
+            talalat = 1;
+            //--------------------------------------------------------------------------------
+            //***********************************************************   keres ciklus end
+        } //talï¿½latvizsgï¿½lat ciklus
+
+        //***************************************************** if end
+    } //click esemï¿½ny kezelï¿½se END
+
+    /////ï¿½s a gyï¿½ztes !!!
+    if (gameOver == 0) {
+        $("#gameGo").text(actualPlayerName[3] + playerName[actualPlayer]);
+        $("#newGame").click(newg);
+        function newg() {
+            location.reload();
+        }
     }
-    if (actualPlayer == 1) {
-      $("#b" + numberIndex + ".box").css("background-color", "cornflowerblue");
-      $("#b" + numberIndex + ".box").text("O")
-      actualRound++;
-    }
-    actualPlayer = actualRound % 2; //az aktuális játékos 0 vagy 1
-  } else {//ha már felhasznált a mezo figy. üzenet
-    $("#gameGo").text(actualPlayerName[2]);
-  }
-	
-//Találatviszgálat ***********************************
-	keres=true
-	novel=1; //zero novel 2x talalja sajat magat !
-	talalat=1;
-	jel=data[numberIndex];
-	console.log("jel. "+jel);
-	  console.log("jel-jel: "+playGround[ycoor][xcoor+i]+" <---> "+jel+" szamlalo  "+novel+" novelt vizsgalando: "+(xcoor+novel));
-	
-	while (keres==true) {//találatvizsgálat ciklus
-//***********************************************************   keres ciklus start	
-		
-//--------------------------------------------------------------------------------		
-///////keres jobbra xcoor+1 !!! tehát balra tedd az elemet !!!		
-		 while ((playGround[xcoor+novel][ycoor])==jel){
-			 talalat++;
-			 novel++;
-			 if (talalat==5){
-				 console.log("**************+++++++++++++******* HEUREKAAA *****");
-				 gameOver=0;
-			 }
-		 }
-		novel=1;
-///////keres ballra xcoor-1
-		    while ((playGround[xcoor-novel][ycoor])==jel){
-		 talalat++;
-			 novel++;
-			 if (talalat==5){
-				 console.log("**************+++++++++++++******* HEUREKAAA *****");
-				 gameOver=0;
-			 }
-		 }	
-		 console.log("talalat: "+talalat+"  keres: "+keres);
-		 keres=false;
-		 console.log("talalat: "+talalat+"  keres: "+keres);
-		novel=1;
-		talalat=1;
-//--------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------		
-///////keres fugg fel ycoor+1
-		 while ((playGround[xcoor][ycoor+novel])==jel){
-			 talalat++;
-			 novel++;
-			 if (talalat==5){
-				 console.log("**************+++++++++++++******* HEUREKAAA *****");
-				 gameOver=0;
-			 }
-		 }
-		novel=1;
-///////keres fugg le ycoor-1
-		    while ((playGround[xcoor][ycoor-novel])==jel){
-		 talalat++;
-			 novel++;
-			 if (talalat==5){
-				 console.log("**************+++++++++++++******* HEUREKAAA *****");
-				 gameOver=0;
-			 }
-		 }	
-		 console.log("talalat: "+talalat+"  keres: "+keres);
-		 keres=false;
-		 console.log("talalat: "+talalat+"  keres: "+keres);
-		novel=1;
-		talalat=1;
-//--------------------------------------------------------------------------------	
-//--------------------------------------------------------------------------------		
-///////keres jobbra le
-		 while ((playGround[xcoor+novel][ycoor-novel])==jel){
-			 talalat++;
-			 novel++;
-			 if (talalat==5){
-				 console.log("**************+++++++++++++******* HEUREKAAA *****");
-				 gameOver=0;
-			 }
-		 }
-		novel=1;
-///////keres ballra fel
-		    while ((playGround[xcoor-novel][ycoor+novel])==jel){
-		 talalat++;
-			 novel++;
-			 if (talalat==5){
-				 console.log("**************+++++++++++++******* HEUREKAAA *****")
-			 }
-		 }	
-		 console.log("talalat: "+talalat+"  keres: "+keres);
-		 keres=false;
-		 console.log("talalat: "+talalat+"  keres: "+keres);
-		novel=1;
-		talalat=1;
-//--------------------------------------------------------------------------------	
-//--------------------------------------------------------------------------------		
-///////keres ballra le	
-		 while ((playGround[xcoor-novel][ycoor-novel])==jel){
-			 talalat++;
-			 novel++;
-			 if (talalat==5){
-				 console.log("**************+++++++++++++******* HEUREKAAA *****");
-				 gameOver=0;
-			 }
-		 }
-		novel=1;
-///////keres jobbra fel
-		    while ((playGround[xcoor+novel][ycoor+novel])==jel){
-		 talalat++;
-			 novel++;
-			 if (talalat==5){
-				 console.log("**************+++++++++++++******* HEUREKAAA *****");
-				 gameOver=0;
-			 }
-		 }	
-		 console.log("talalat: "+talalat+"  keres: "+keres);
-		 keres=false;
-		 console.log("talalat: "+talalat+"  keres: "+keres);
-		novel=1;
-		talalat=1;
-//--------------------------------------------------------------------------------	
-//***********************************************************   keres ciklus end	
-
-	}//találatvizsgálat ciklus
- 
-//***************************************************** if end
-	
-	}//click esemény kezelése END
-	
-/////és a gyöztes !!!
-	if(gameOver==0){
-		$("#gameGo").text(actualPlayerName[3]+playerName[actualPlayer]);
-		$("#newGame").click(newg);
-		function newg(){location.reload();};
-	}
-})
+});
